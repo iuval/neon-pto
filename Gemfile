8dca3118eb2ec99ba3cb96e7cd19cbd22baf1750
iuval <iuval.goldansky@neonroots.com>
2014-01-02 09:48:11 -0200

    Add rails_12factor gem for production assets in heroku
    Change url helper for fonts from url to font-url
    
    Change bootstrap.css to scss to user font-url helper
    
    Changes font-url() to url(font-path())
    
    Add config.assets.compile = true
    
    Remove bootstrap files. Add bootstrap-sass-rails gem
    
    Change bootstrap-sass-rails to bootstrap-sass
    
    Change application.css to .csss
    
    Rollback to heroku files

--- Gemfile
+++ Gemfile
@@ -48,6 +48,7 @@ end
 
 group :production do
   gem 'thin'
+  gem 'rails_12factor'
 end
 
 group :test do
--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ module NeonPto
     # -- all .rb files in that directory are automatically loaded.
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
+    # Run 'rake -D time' for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
@@ -22,5 +22,9 @@ module NeonPto
     config.assets.initialize_on_precompile = false
 
     config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+    config.assets.precompile += ['gliphicons-halflings-regular.ttf',
+                         'gliphicons-halflings-regular.eot',
+                         'gliphicons-halflings-regular.svg',
+                         'gliphicons-halflings-regular.woff']
   end
 end
