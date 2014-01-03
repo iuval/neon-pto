abcb09f4f8afb497820bfab123ea0648d60edbed
iuval <iuval.goldansky@neonroots.com>
2014-01-03 09:12:27 -0200

    Changes carrierwave storage depending on env

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -7,8 +7,7 @@ class PictureUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  storage CarrierWave::Uploader::EnvironmentStorage.for(ENV['RAILS_ENV'])
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
